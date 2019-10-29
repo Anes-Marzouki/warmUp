728e966c578ac8ed9c751d4fdeac60d2afdaa28d
Anes Marzouki <marzoukianas@gmail.com>
2019-10-29 10:13:56 +0100

    creates a files warmUp10 with index js and html with stylesheet

---
+++ warmUp10/index.js
@@ -0,0 +1,9 @@
+/*
+var text = document.getElementsByTagName('input');
+function createsItem () {
+	$('button').on.('click','text...' );
+	$('ul').append('<li>...</li>');
+	$('li')....(.class1)
+
+}
+*/
